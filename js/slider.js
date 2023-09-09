b9d0e01866fe6c8f74e9bee5b872871eff5bd764
Streckk <hedelgovea3@gmail.com>
2023-09-09 00:59:03 -0600

    version 2.0

---
+++ js/slider.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.swiper', {
+    slidesPerView: 3, // Número de diapositivas visibles a la vez
+    spaceBetween: 20, // Espacio entre diapositivas
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+});
